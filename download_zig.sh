cc231a09499f7c173454107f004239c86269bd18
Meghan <hello@nektro.net>
2020-11-11 18:54:10 -0800

    move zig download to script file

---
+++ download_zig.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+os="linux"
+arch="x86_64"
+version="$1"
+
+dir="zig-$os-$arch-$version"
+file="$file.tar.xz"
+
+cd ~
+apk add wget tar
+wget https://ziglang.org/download/$version/$file
+tar -vxf $file
+ln -s ~/$dir/zig /bin

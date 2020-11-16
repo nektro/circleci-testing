ff83d6763fed53efba2be6954b81dc8c2914e268
Meghan <hello@nektro.net>
2020-11-16 11:35:24 -0800

    download_zig- switch to root

--- download_zig.sh
+++ download_zig.sh
@@ -3,6 +3,8 @@
 set -x
 set -e
 
+sudo su
+
 os="linux"
 arch="x86_64"
 version="$1"
@@ -10,7 +12,7 @@ version="$1"
 dir="zig-$os-$arch-$version"
 file="$dir.tar.xz"
 
-cd ~
+cd /
 wget https://ziglang.org/download/$version/$file
 tar -vxf $file
-ln -s ~/$dir/zig ~/.local/bin
+ln -s /$dir/zig ~/.local/bin
